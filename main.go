66cd862d89be8d22268f5a0a9d6bfb9946a2883d
shifat23 <shifat@revo-interactive.com>
2025-09-30 18:58:48 +0600

    basic commit for a series update

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"html/template"
 	"net/http"
 )
 
+var tpl *template.Template
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
-	})
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	port := ":8080"
-	fmt.Printf("Server starting on port %s\n", port)
+	tpl, _ = tpl.ParseGlob("templates/*.html")
 
-	log.Fatal(http.ListenAndServe(port, nil))
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/dashboard", dashboardHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	err := tpl.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		return
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	err := tpl.ExecuteTemplate(w, "login.html", nil)
+	if err != nil {
+		return
+	}
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	err := tpl.ExecuteTemplate(w, "register.html", nil)
+	if err != nil {
+		return
+	}
+}
+
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	err := tpl.ExecuteTemplate(w, "dashboard.html", nil)
+	if err != nil {
+		return
+	}
 }
